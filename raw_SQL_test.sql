290c6054aa46c2d6065a5fa746a9ae4f25d86579
Sumaia Anjum Shaba <91652076+Shaba413@users.noreply.github.com>
2021-12-05 01:52:09 +0600

    Dev (#40)
    * Update raw_SQL_test.sql
    
    * Update raw_SQL_test.sql
    
    * IUB available resources
    
    Co-authored-by: nity123 <91755203+nity123@users.noreply.github.com>
    Co-authored-by: Fayeeza23 <91619757+Fayeeza23@users.noreply.github.com>

--- raw_SQL_test.sql
+++ raw_SQL_test.sql
@@ -8,5 +8,52 @@ WHERE SchoolTitle_id="SETS" AND Semester="Summer" AND Year=2021 AND SectionEnrol
 
 
 
---
+-- ClassSize
+SELECT COUNT(*)
+FROM seasapp_section_t 
+WHERE SectionCapacity BETWEEN 1 AND 10
+AND semester = "Summer"
+AND YEAR =2021;
+ ---- usage of the resources spring table
+ SELECT AVG(RoomCapacity) AS Spring
+FROM seasapp_room_t, seasapp_section_t
+WHERE Semester = "Spring"
+
+UNION
+
+SELECT AVG(SectionEnrolled)
+FROM  seasapp_section_t
+WHERE Semester = "Summer"
+
+UNION
  
+SELECT AVG(RoomCapacity)- AVG(SectionEnrolled)  
+FROM seasapp_room_t, seasapp_section_t
+UNION
+
+SELECT (( AVG(RoomCapacity)- AVG(SectionEnrolled))/AVG(RoomCapacity))*100
+FROM seasapp_room_t, seasapp_section_t
+
+-- Enrolment table for every school
+SELECT SectionEnrolled, COUNT(*) AS SBE
+FROM seasapp_course_t c 
+INNER JOIN seasapp_department_t d ON c.DeptID_id = d.DeptID
+INNER JOIN seasapp_school_t s ON s.SchoolTitle = d.SchoolTitle_id 
+INNER JOIN seasapp_section_t sec ON sec.CourseID_id = c.CourseID 
+WHERE Schooltitle ="SBE" AND Semester="Spring" AND SectionEnrolled BETWEEN 1 AND 10
+GROUP BY SectionEnrolled
+HAVING SectionEnrolled > 0
+ORDER BY SectionEnrolled ASC;
+ -- For checking 
+--  SELECT* FROM seasapp_course_t c
+-- INNER JOIN seasapp_department_t d ON c.DeptID_id = d.DeptID
+-- INNER JOIN seasapp_school_t s ON s.SchoolTitle = d.SchoolTitle_id 
+-- INNER JOIN seasapp_section_t sec ON sec.CourseID_id = c.CourseID 
+-- WHERE SectionEnrolled = "10" AND Semester="Spring" AND SchoolTitle="SBE"
+
+
+-- IUB available resources
+SELECT RoomCapacity AS ClassSize, Count(*) AS IUB_Resourse , RoomCapacity*Count(*) AS Capacity
+FROM seasapp_section_t S INNER JOIN seasapp_room_t R ON S.RoomID_id = R.RoomID
+GROUP BY RoomCapacity 
+ORDER BY RoomCapacity ;
