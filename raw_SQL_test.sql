a199a26decc664bf3955bfd487dfb90010510676
Fayeeza23 <91619757+Fayeeza23@users.noreply.github.com>
2021-12-04 21:59:23 +0600

    Update raw_SQL_test.sql

--- raw_SQL_test.sql
+++ raw_SQL_test.sql
@@ -8,5 +8,19 @@ WHERE SchoolTitle_id="SETS" AND Semester="Summer" AND Year=2021 AND SectionEnrol
 
 
 
---
- 
+-- Enrolment table for every school
+SELECT SectionEnrolled, COUNT(*) AS SBE
+FROM seasapp_course_t c 
+INNER JOIN seasapp_department_t d ON c.DeptID_id = d.DeptID
+INNER JOIN seasapp_school_t s ON s.SchoolTitle = d.SchoolTitle_id 
+INNER JOIN seasapp_section_t sec ON sec.CourseID_id = c.CourseID 
+WHERE Schooltitle ="SBE" AND Semester="Spring" AND SectionEnrolled BETWEEN 1 AND 10
+GROUP BY SectionEnrolled
+HAVING SectionEnrolled > 0
+ORDER BY SectionEnrolled ASC;
+ -- For checking 
+--  SELECT* FROM seasapp_course_t c
+-- INNER JOIN seasapp_department_t d ON c.DeptID_id = d.DeptID
+-- INNER JOIN seasapp_school_t s ON s.SchoolTitle = d.SchoolTitle_id 
+-- INNER JOIN seasapp_section_t sec ON sec.CourseID_id = c.CourseID 
+-- WHERE SectionEnrolled = "10" AND Semester="Spring" AND SchoolTitle="SBE"
