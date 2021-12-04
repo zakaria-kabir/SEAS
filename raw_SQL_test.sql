c31d9d430cc97a18dca8d6069af53c24cb06dde4
Md. Zakaria Kabir <zakariak5469@gmail.com>
2021-12-04 18:50:27 +0600

    rawsql add

---
+++ raw_SQL_test.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM seasapp_department_t AS D INNER JOIN seasapp_course_t AS C ON DeptID=DeptID_id
+                               INNER JOIN seasapp_section_t AS S ON CourseID=CourseID_id
+
+WHERE Semester="Summer" AND Year=2021 AND SectionEnrolled BETWEEN 1 AND 10;
+#LIMIT 50;
+                               
